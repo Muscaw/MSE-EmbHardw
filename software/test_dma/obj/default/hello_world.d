4f7386b32585373da1e25f3239f97ae75ea05ae3
Kevin Grandjean <moi@kevingrandjean.ch>
2017-11-08 14:04:10 +0100

    Added dma software

---
+++ software/test_dma/obj/default/hello_world.d
@@ -0,0 +1,21 @@
+obj/default/hello_world.o: hello_world.c ../test_dma_bsp//HAL/inc/io.h \
+ ../test_dma_bsp//HAL/inc/alt_types.h \
+ ../test_dma_bsp//HAL/inc/sys/alt_irq.h ../test_dma_bsp//HAL/inc/nios2.h \
+ ../test_dma_bsp//HAL/inc/alt_types.h ../test_dma_bsp/system.h \
+ ../test_dma_bsp/linker.h Image.c
+
+../test_dma_bsp//HAL/inc/io.h:
+
+../test_dma_bsp//HAL/inc/alt_types.h:
+
+../test_dma_bsp//HAL/inc/sys/alt_irq.h:
+
+../test_dma_bsp//HAL/inc/nios2.h:
+
+../test_dma_bsp//HAL/inc/alt_types.h:
+
+../test_dma_bsp/system.h:
+
+../test_dma_bsp/linker.h:
+
+Image.c:
